39ad2228733d7830cace006dd437a84ceb22d352
Kentout26 <126180066+Kentout26@users.noreply.github.com>
2023-04-17 17:10:22 +0300

    fix update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+    let SR = 0.693 / HALF_LIFE_PERIOD
+    let number = +sampleActivity;
+    if (typeof sampleActivity === string || number > 15 || number < 1){
+        return false;
+    }
+    return Math.ceil(Math.log(MODERN_ACTIVITY / number) / speedReaction);
 }
 
 module.exports = {
